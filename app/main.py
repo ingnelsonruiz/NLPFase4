30084446a355d9e17a3293e07f9da126c39fb785
ingnelsonruiz <ingenieronelson@hotmail.com>
2026-01-03 16:26:55 -0500

    Fix import statement for RetrievalQA

--- app/main.py
+++ app/main.py
@@ -10,7 +10,8 @@ from langchain_community.document_loaders import TextLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 # IMPORTACIÓN CORRECTA:
-from langchain.chains.retrieval_qa.base import RetrievalQA
+# from langchain.chains.retrieval_qa.base import RetrievalQA
+from langchain.chains import RetrievalQA
 
 
 # =========================
